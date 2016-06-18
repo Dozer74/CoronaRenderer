863e6573c1185976210688ff4b66d1b5fc9a10bd
Евгений <Dozer95@bk.ru>
2016-06-18 21:29:22 +0500

    поправлен импорт maxconnect

--- scriptRunner.py
+++ scriptRunner.py
@@ -1,4 +1,8 @@
 import os
+import sys
+
+path = os.path.realpath('maxconnect')
+sys.path.append(path)
 import maxconnect
 
 filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'main.py')
