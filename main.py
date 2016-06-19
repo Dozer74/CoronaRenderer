b0396b9cb112be1c049f3895041aa573ef59f13c
Евгений <Dozer95@bk.ru>
2016-06-19 22:22:09 +0500

    Изменено чтение параметров

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ scriptNoiseLimit = 'renderers.current.adaptivity_targetError'
 scriptTimeLimit = 'renderers.current.progressive_timeLimit'
 
 
-def setCoronaRenderer():
+def set_coronarenderer():
     """
     Устанавливает Corona Renderer, если был выбран другой движок
     """
@@ -23,7 +23,7 @@ def setCoronaRenderer():
         return False
 
 
-def getLimits():
+def get_limits():
     """
     Получает значения всех трех лимитов Corona Renderer'a
     Возвращает кортеж из них
@@ -33,43 +33,61 @@ def getLimits():
             MaxPlus.Core.EvalMAXScript(scriptTimeLimit).GetInt64())
 
 
-def setLimits(passLim=0, noiseLim=0.0, timeLim=0):
+def set_limits(passLim=-1, noiseLim=-1.0, timeLim=-1):
     """
     Устанавливает значения лимитов
 
     Несмотря на 'The format is HOURS:MINUTES:SECONDS' из документации,
     значение timeLimit задается в мс!
     """
-    maxscript = '{0} = {1}\n'.format(scriptPassLimit, passLim) \
-                + '{0} = {1}\n'.format(scriptNoiseLimit, noiseLim) \
-                + '{0} = {1}\n'.format(scriptTimeLimit, timeLim*1000) # timeLimit в мс
-    MaxPlus.Core.EvalMAXScript(maxscript)
+    maxscript = ''
+    if int(passLim)>=0:
+        maxscript += '{0} = {1}'.format(scriptPassLimit, passLim)
+    if float(noiseLim)>=0:
+        maxscript += '{0} = {1}'.format(scriptNoiseLimit, noiseLim)
+    if int(timeLim)>=0:
+        maxscript += '{0} = {1}'.format(scriptTimeLimit, int(timeLim)*1000) # timeLimit в мс
+
+    if maxscript:
+        MaxPlus.Core.EvalMAXScript(maxscript)
 
 
-def writeLimitsToFile(path, limits):
+def write_to_file(path, limits):
     f = open(path, 'w')
     f.write("PassLimit: {}\n".format(limits[0]))
     f.write("NoiseLimit: {}\n".format(limits[1]))
-    f.write("TimeLimit: {} (сек)".format(limits[2]))
+    f.write("TimeLimit: {} (sec)".format(limits[2] / 1000))
     f.close()
 
 
-def readParams():
+def read_params():
     """
     производит чтение параметров из файла. Затем файл удаляется
     """
     paramsPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'params.txt')
     f = open(paramsPath,'r')
-    data = f.readlines()
+    data = f.readlines()[1:] #пропускаем команду get/set
     f.close()
     os.remove(paramsPath)
     return data
 
 
-passLim, noiseLim, timeLim, path = readParams() # получаем значения параметров
-if setCoronaRenderer(): # пытаемся установить CoronaRenderer в качестве рендер-движка
-    setLimits(passLim, noiseLim, timeLim) # устанавливаем значения лимитов
-    limits = getLimits() # получаем значения лимитов
-    writeLimitsToFile(path, limits) # сохраняем результаты в файл
+def read_command():
+    paramsPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'params.txt')
+    f = open(paramsPath, 'r')
+    data = f.readline().replace('\n', '')
+    f.close()
+    return data
+
+
+command = read_command()
+if set_coronarenderer():
+    if command=='get':
+        path = read_params()[0]
+        limits = get_limits()  # получаем текущие значения лимитов
+        write_to_file(path, limits)  # сохраняем результаты в файл
+    else:
+        passLim, noiseLim, timeLim = read_params()
+        set_limits(passLim, noiseLim, timeLim)  # устанавливаем новые значения
 else:
     print "Corona renderer not found!"
--- scriptRunner.py
+++ scriptRunner.py
@@ -6,33 +6,47 @@ import argparse
 sys.path.append(os.path.realpath('maxconnect'))
 import maxconnect
 
+parampath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'params.txt')
 
-def readArgs():
+
+def get_limits(args):
+    f = open(parampath,'w')
+    f.writelines(['get\n', args.path])
+    f.close()
+
+
+def set_limits(args):
+    f = open(parampath, 'w')
+    f.writelines(['set\n',
+                  str(args.passLimit)+'\n',
+                  str(args.noiseLimit)+'\n',
+                  str(args.timeLimit)+'\n'])
+    f.close()
+
+
+def read_args():
     """
     производит чтнение аргументов из командной строки
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument("-t", "--timeLimit", type=int, help="sets time limit in sec", default=0)
-    parser.add_argument("-n", "--noiseLimit", type=float, help="sets noise limit", default=0)
-    parser.add_argument("-p", "--passLimit", type=int, help="sets pass limit", default=0)
-    parser.add_argument("-rp", "--resultsPath", type=str, help="sets path to file with results",
-                        default=os.path.expanduser("~/Desktop/results.txt"))
+    subparsers = parser.add_subparsers(help='List of commands')
+    # get parser
+    getlimits_parser = subparsers.add_parser('get', help='Get the values of limits')
+    getlimits_parser.add_argument('-path', dest='path', type=str,
+                                  help='Path to file with result',
+                                  default=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'limits.txt'))
+    getlimits_parser.set_defaults(func=get_limits)
+    # set parser
+    setlimits_parser = subparsers.add_parser('set', help='Set the values of limits')
+    setlimits_parser.add_argument('-p', '--passLimit', type=int, default=-1, help='Pass limit value')
+    setlimits_parser.add_argument('-n', '--noiseLimit', type=float, default=-1.0, help='Noise limit value')
+    setlimits_parser.add_argument('-t', '--timeLimit', type=int, default=-1, help='Time limit value (in seconds)')
+    setlimits_parser.set_defaults(func=set_limits)
     return parser.parse_args()
 
 
-def writeArgsToFile(args):
-    """
-    записывает аргументы в файл. После чтения в main.py файл удаляется
-    """
-    path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'params.txt')
-    f = open(path, 'w')
-    f.writelines(
-        [str(args.passLimit) + '\n', str(args.noiseLimit) + '\n', str(args.timeLimit) + '\n', args.resultsPath])
-    f.close()
-
-
-args = readArgs()
-writeArgsToFile(args)
+args = read_args()
+args.func(args)
 filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'main.py')
 cmd = r'python.ExecuteFile @"%s";' % filename
 maxconnect.scriptrunner.run(cmd)
